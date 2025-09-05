62ee117477a6dc457f08c2c72fa993088f75d85b
Kanan Farzaliyev <kanan.farzali@gmail.com>
2025-09-05 17:08:50 +0200

    fix home page

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react'
 import { Link, useNavigate } from 'react-router'
-
 import Navbar from '~/components/Navbar'
 import ResumeCard from '~/components/ResumeCard'
 import { usePuterStore } from '~/lib/puter'
@@ -13,7 +12,7 @@ export function meta({}: Route.MetaArgs) {
   ]
 }
 
-const Home = () => {
+export default function Home() {
   const { auth, kv } = usePuterStore()
   const navigate = useNavigate()
   const [resumes, setResumes] = useState<Resume[]>([])
@@ -27,13 +26,11 @@ const Home = () => {
     const loadResumes = async () => {
       setLoadingResumes(true)
 
-      const resumes = (await kv.list('resume:*')) as KVItem[]
+      const resumes = (await kv.list('resume:*', true)) as KVItem[]
 
       const parsedResumes = resumes?.map(
         resume => JSON.parse(resume.value) as Resume
       )
-
-      console.log(parsedResumes)
 
       setResumes(parsedResumes || [])
       setLoadingResumes(false)
@@ -49,16 +46,15 @@ const Home = () => {
       <section className="main-section">
         <div className="page-heading py-16">
           <h1>Track Your Applications & Resume Ratings</h1>
-          {!loadingResumes && resumes.length === 0 ? (
-            <h2>No resumes found. Upload a resume to get started.</h2>
+          {!loadingResumes && resumes?.length === 0 ? (
+            <h2>No resumes found. Upload your first resume to get feedback.</h2>
           ) : (
             <h2>Review your submissions and check AI-powered feedback.</h2>
           )}
         </div>
-
         {loadingResumes && (
           <div className="flex flex-col items-center justify-center">
-            <img src="/images/resume-scan-2.gif" alt="" className="w-[200px]" />
+            <img src="/images/resume-scan-2.gif" className="w-[200px]" />
           </div>
         )}
 
@@ -84,5 +80,3 @@ const Home = () => {
     </main>
   )
 }
-
-export default Home
