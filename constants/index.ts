d866a1879625af53db54168f010fd956346e5ce4
Kanan Farzaliyev <kanan.farzali@gmail.com>
2025-09-08 22:29:38 +0200

    cleanup

--- constants/index.ts
+++ constants/index.ts
@@ -182,7 +182,7 @@ export const resumes: Resume[] = [
 ]
 
 export const AIResponseFormat = `
-      interface Feedback {
+    interface Feedback {
       overallScore: number; //max 100
       ATS: {
         score: number; //rate based on ATS suitability
