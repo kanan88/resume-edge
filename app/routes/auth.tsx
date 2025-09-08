f688d045dda1ebb82b1e11d7ab3ca3f00b0a07aa
Kanan Farzaliyev <kanan.farzali@gmail.com>
2025-09-09 00:40:08 +0200

    fix auth page

--- app/routes/auth.tsx
+++ app/routes/auth.tsx
@@ -2,7 +2,7 @@ import { useEffect } from 'react'
 import { useLocation, useNavigate } from 'react-router'
 import { usePuterStore } from '~/lib/puter'
 
-export const meta = ({}) => [
+export const meta = () => [
   { title: 'Resume Edge | Auth' },
   { name: 'description', content: 'Log into your account' }
 ]
@@ -18,12 +18,12 @@ const Auth = () => {
   }, [auth.isAuthenticated, next])
 
   return (
-    <main className="bg-[url('/images/bg-main.svg')] bg-cover min-h-screen flex items-center justify-center">
+    <main className="bg-[url('/images/bg-auth.svg')] bg-cover min-h-screen flex items-center justify-center">
       <div className="gradient-border shadow-lg">
         <section className="flex flex-col gap-8 bg-white rounded-2xl p-10">
           <div className="flex flex-col items-center gap-2 text-center">
             <h1>Welcome</h1>
-            <h2>Log in to Continue Your Job Journey</h2>
+            <h2>Log In to Continue Your Job Journey</h2>
           </div>
           <div>
             {isLoading ? (
@@ -34,7 +34,7 @@ const Auth = () => {
               <>
                 {auth.isAuthenticated ? (
                   <button className="auth-button" onClick={auth.signOut}>
-                    Log Out
+                    <p>Log Out</p>
                   </button>
                 ) : (
                   <button className="auth-button" onClick={auth.signIn}>
